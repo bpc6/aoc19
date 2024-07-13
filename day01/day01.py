67647c30a6b1086a45e3c36b9204617aa6a3b63c
Bryan Covell <bpc6@vt.edu>
2024-07-12 19:37:47 -0600

    finished day01

---
+++ day01/day01.py
@@ -0,0 +1,23 @@
+def get_fuel(mass: int) -> int:
+    return int(mass / 3) - 2
+
+
+def get_fuel_recursive(mass: int) -> int:
+    if mass <= 0:
+        return 0
+    return mass + get_fuel_recursive(get_fuel(mass))
+
+
+def get_total_fuel(mass: int) -> int:
+    return get_fuel_recursive(mass) - mass
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        lines = f.readlines()
+
+    total = 0
+    for line in lines:
+        total += get_total_fuel(int(line.strip()))
+
+    print(total)
